ae02865d9cdf8883cd47fb713e0981d8508a389e
Jayshil Dave <jayshildave@gmail.com>
2015-12-27 11:34:19 +0530

    Removed unit test cases
    Removed unit test cases

--- app/src/main/java/com/jd/app/android/expensetracker/DashboardActivity.java
+++ app/src/main/java/com/jd/app/android/expensetracker/DashboardActivity.java
@@ -14,30 +14,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-//        Expense expense = new Expense();
-//        expense.setDate("2015/11/14");
-//        expense.setTime("20:03");
-//        expense.setExpenseType(Expense.ExpenseType.GIFTS);
-//        expense.setPrice(200.00);
-//
-//        Log.d(TAG, "Expense created is " + expense.toString());
-//
-//        expense.save();
-//
-//        Expense expense1 = Expense.findById(Expense.class, 1L);
-//
-//        Log.d(TAG, "Expense retrieved is " + expense1.toString());
-//
-//        expense1.setPrice(3000.00);
-//
-//        Log.d(TAG, "Expense modified is " + expense1.toString());
-//
-//        expense1.save();
-
-        Expense expense2 = Expense.findById(Expense.class, 1L);
-
-        Log.d(TAG, "Expense retrieved post modification is " + expense2.toString());
     }
 
     public int add() {
